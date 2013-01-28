3163f6634880347d48050e6b269a9e1c5eabe5fb
hiroyuki12 <hiroyuki12@gmail.com>
2013-01-29 01:35:12 +0900

    fix tab

--- src/android/sample/BookmarkActivity.java
+++ src/android/sample/BookmarkActivity.java
@@ -32,7 +32,8 @@ public class BookmarkActivity extends Activity {
 	private String deleteUrl;
 	private int deletePosition;
 	
-    @Override protected void onCreate(Bundle savedInstanceState) {
+	@Override 
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);   //既存のコード
 		requestWindowFeature(Window.FEATURE_NO_TITLE);  //タイトルバーを非表示にする
 		setContentView(R.layout.activity_main);  //既存のコード
@@ -45,11 +46,11 @@ public class BookmarkActivity extends Activity {
 		
 		// ブックマークファイルを読み込みリストビューにセット
 		Read();
-        
+		
 		//リスト項目がクリックされた時の処理
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-	        @Override
-	        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				ListView listView = (ListView) parent;
 				String item = (String) listView.getItemAtPosition(position);  // title, url
 				String[] url = item.split(",");
@@ -67,160 +68,159 @@ public class BookmarkActivity extends Activity {
 				}
 				// finish() で終わらせてIntent data を送る
 				finish();
-	        }	
-        });
-         
-       listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-    	   //リスト項目が選択された時の処理
-          @Override
-          public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-              ListView listView = (ListView) parent;
-              String item = (String) listView.getItemAtPosition(position);
-              Toast.makeText(getApplicationContext(), item + " selected",
-              	Toast.LENGTH_LONG).show();
-          	}
-          
-            //リスト項目がなにも選択されていない時の処理
-          @Override
-          public void onNothingSelected(AdapterView<?> parent) {
-              Toast.makeText(getApplicationContext(), "no item selected",
-                      Toast.LENGTH_LONG).show();
-            }
-        });
-         
-        // コンテキストメニューのためにリストビューを登録
-       registerForContextMenu(listView);
-    }
-
-    // ブックマークファイルを読み込みリストビューにセット
-    public void Read()
-    {
-    	FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = openFileInput(bookmarkFileName);
-            byte[] readBytes = new byte[fileInputStream.available()];
-            fileInputStream.read(readBytes);
-            String readString = new String(readBytes);
-            bookmarks = readString;
-            Log.v("readString", readString);
-
-            // XMLで定義したandroid:idの値を指定してListViewを取得します。
-            listView = (ListView) findViewById(R.id.listView1);
-            // ListViewに表示する要素を保持するアダプタを生成します。
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                                    android.R.layout.simple_list_item_1);
-            // 要素を追加
-            String[] bookmark = bookmarks.split("\n");
-            for( String data : bookmark ) {
-            	arrayAdapter.add(data);
-            }
-            // 選択行の色を変えるように設定
-            //listView.setSelector(new PaintDrawable(Color.LTGRAY));
-            // アダプタを設定
-            listView.setAdapter(arrayAdapter);
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-        }
-        finally {
-            if (fileInputStream != null) {
-            	try{
-            		fileInputStream.close();
-            	}
-            	catch(Exception ex) {
-            	}
-            }
-        }
-    }
-    
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-    		AdapterContextMenuInfo adapterinfo = (AdapterContextMenuInfo)menuInfo;
-    		ListView listView = (ListView)v;
-
-    		deleteUrl = (String)listView.getItemAtPosition(adapterinfo.position);
-    		deletePosition = adapterinfo.position;
-    		// コンテキストメニューを作る
-    		menu.setHeaderTitle(deleteUrl);  //ListViewのURL
-    		//menu.add(R.string.context_delete);
-    		menu.add("削除");
-
-    		super.onCreateContextMenu(menu, v, menuInfo);
-    }
-    
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-    	if(item.getTitle()=="削除")
-    	{
-    		Delete();
-    		
-	        Toast.makeText(getApplicationContext(), "削除: " + deleteUrl,
-	                Toast.LENGTH_SHORT).show();
-    	}
-        return super.onContextItemSelected(item);
-    }
-    
-    public void Delete()
-    {
-    	// 改行で分解
-        String[] bookmark = bookmarks.split("\n");
-
-    	FileOutputStream fileOutputStream = null;
-    	
-    	try {
-    		int i=0;
-    		//削除したブックマーク以外をファイルに書き込む
-        	for( String data : bookmark )
-        	{
-        		if(i != deletePosition)
-        		{
-	        		if(i==0 || (deletePosition==0 && i==1))
-	        		{
-	                    fileOutputStream = openFileOutput(bookmarkFileName, MODE_PRIVATE); 
-	                    String writeString = data + "\n";
-	                    fileOutputStream.write(writeString.getBytes());
-	        		}
-	        		else
-	        		{            
-	    			    fileOutputStream = openFileOutput(bookmarkFileName, MODE_APPEND);  //追記モード
-	                    String writeString = data + "\n";
-	                    fileOutputStream.write(writeString.getBytes());
-	        		}
-        		}
-        		
-        		i++;
-        	}
-        	
-        	//削除した後のブックマークファイルを読み込む
-        	Read();
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-        }
-        finally {
-            if (fileOutputStream != null) {
-            	try{
-            	fileOutputStream.close();
-            	}
-            	catch(Exception ex) {
-            	}
-            }
-        }
-    }
-    
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-    	//if(mLongPressed){
-    		// 長押しされた時の処理
-    	//}else if(!mLongPressed){
-    		// 単押しされた時の処理
-			if(keyCode == KeyEvent.KEYCODE_BACK){
-	            // 第一引数は…Activity.RESULT_OK,
-	            // Activity.RESULT_CANCELED など
-	            setResult(RESULT_CANCELED, null);
-				// 戻るキーが押された時の処理
-				finish(); // 戻るキーが長押しされたらブックマーク画面を終了
+			}	
+		});
+		
+		listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			//リスト項目が選択された時の処理
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				ListView listView = (ListView) parent;
+				String item = (String) listView.getItemAtPosition(position);
+				Toast.makeText(getApplicationContext(), item + " selected",
+						Toast.LENGTH_LONG).show();
 			}
-    	//}
-    	
-    	return super.onKeyUp(keyCode, event);
-    }
+			
+			//リスト項目がなにも選択されていない時の処理
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				Toast.makeText(getApplicationContext(), "no item selected",
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		// コンテキストメニューのためにリストビューを登録
+		registerForContextMenu(listView);
+	}
+	
+	// ブックマークファイルを読み込みリストビューにセット
+	public void Read()
+	{
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = openFileInput(bookmarkFileName);
+			byte[] readBytes = new byte[fileInputStream.available()];
+			fileInputStream.read(readBytes);
+			String readString = new String(readBytes);
+			bookmarks = readString;
+			Log.v("readString", readString);
+			
+			// XMLで定義したandroid:idの値を指定してListViewを取得します。
+			listView = (ListView) findViewById(R.id.listView1);
+			// ListViewに表示する要素を保持するアダプタを生成します。
+			ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+					android.R.layout.simple_list_item_1);
+			// 要素を追加
+			String[] bookmark = bookmarks.split("\n");
+			for( String data : bookmark ) {
+				arrayAdapter.add(data);
+			}
+			// 選択行の色を変えるように設定
+			//listView.setSelector(new PaintDrawable(Color.LTGRAY));
+			// アダプタを設定
+			listView.setAdapter(arrayAdapter);
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		finally {
+			if (fileInputStream != null) {
+				try{
+					fileInputStream.close();
+				}
+				catch(Exception ex) {
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		AdapterContextMenuInfo adapterinfo = (AdapterContextMenuInfo)menuInfo;
+		ListView listView = (ListView)v;
+		
+		deleteUrl = (String)listView.getItemAtPosition(adapterinfo.position);
+		deletePosition = adapterinfo.position;
+		// コンテキストメニューを作る
+		menu.setHeaderTitle(deleteUrl);  //ListViewのURL
+		//menu.add(R.string.context_delete);
+		menu.add("削除");
+		
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		if(item.getTitle()=="削除")
+		{
+			Delete();
+			
+			Toast.makeText(getApplicationContext(), "削除: " + deleteUrl,
+					Toast.LENGTH_SHORT).show();
+		}
+		return super.onContextItemSelected(item);
+	}
+	
+	public void Delete()
+	{
+		// 改行で分解
+		String[] bookmark = bookmarks.split("\n");
+		
+		FileOutputStream fileOutputStream = null;
+		
+		try {
+			int i=0;
+			//削除したブックマーク以外をファイルに書き込む
+			for( String data : bookmark )
+			{
+				if(i != deletePosition)
+				{
+					if(i==0 || (deletePosition==0 && i==1))
+					{
+						fileOutputStream = openFileOutput(bookmarkFileName, MODE_PRIVATE); 
+						String writeString = data + "\n";
+						fileOutputStream.write(writeString.getBytes());
+					}
+					else
+					{
+						fileOutputStream = openFileOutput(bookmarkFileName, MODE_APPEND);  //追記モード
+						String writeString = data + "\n";
+						fileOutputStream.write(writeString.getBytes());
+					}
+				}
+				i++;
+			}
+				
+			//削除した後のブックマークファイルを読み込む
+			Read();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		finally {
+			if (fileOutputStream != null) {
+				try{
+					fileOutputStream.close();
+				}
+				catch(Exception ex) {
+				}
+			}
+		}
+	}
+	
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		//if(mLongPressed){
+		// 長押しされた時の処理
+		//}else if(!mLongPressed){
+		// 単押しされた時の処理
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+			// 第一引数は…Activity.RESULT_OK,
+			// Activity.RESULT_CANCELED など
+			setResult(RESULT_CANCELED, null);
+			// 戻るキーが押された時の処理
+			finish(); // 戻るキーが長押しされたらブックマーク画面を終了
+		}
+		//}
+		
+		return super.onKeyUp(keyCode, event);
+	}
 }
