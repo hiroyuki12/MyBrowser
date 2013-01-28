e762cecd8728e17a1a41a07431a13847da19d6dd
hiroyuki12 <hiroyuki12@gmail.com>
2013-01-29 00:34:24 +0900

    first add

---
+++ src/android/sample/MainActivity.java
@@ -0,0 +1,299 @@
+package android.sample;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+
+	private WebView webView;
+	private String currentUrl;
+	private String currentTitle;
+	private String bookmarkFileName = "bookmark.txt";
+	
+    @SuppressLint("SetJavaScriptEnabled")
+	@Override protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);   //既存のコード
+    	requestWindowFeature(Window.FEATURE_NO_TITLE);  //タイトルバーを非表示にする
+    	setContentView(R.layout.activity_main);  //既存のコード
+        // Webビューの作成
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setVerticalScrollbarOverlay(true);
+        webView.setWebViewClient(new WebViewClient()  //リンクをタップしたときに標準ブラウザを起動させない
+        {
+            @Override
+            public void onPageStarted(final WebView view, final String url, final Bitmap favicon) {
+            	  //プログレスバーを表示(10%)
+            	  ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+                progressBar.setVisibility(View.VISIBLE);
+            	  progressBar.setProgress(10);
+            	  System.out.println("called onPageStarted");
+            }            
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                  //プログレスバーを非表示
+                ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+                progressBar.setVisibility(View.GONE);
+            	  //ページのURL・タイトルを取得
+            	  currentUrl = url;
+            	  currentTitle = view.getTitle();
+            	  System.out.println("called onPageFinished");
+            }
+        });
+        
+        WebChromeClient chrome = new WebChromeClient() {
+        	public void onProgressChanged(WebView view, int progress) {
+        		//プログレスバー表示を更新
+          	  	ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        		if(progress >= 100)
+        		{
+        			progressBar.setVisibility(View.GONE);
+        		}
+        		else
+        		{
+	          	  	progressBar.setVisibility(View.VISIBLE);
+	          	  	progressBar.setProgress(progress);
+        		}
+          	  	System.out.println("onProgressChanged");
+        	}
+        };
+       webView.setWebChromeClient(chrome);
+       
+		WebSettings settings = webView.getSettings();
+		//settings.setSupportMultipleWindows(true);  //Googleニュースのリンク
+		settings.setLoadsImagesAutomatically(true);
+		//settings.setSupportZoom(true);
+		//settings.setLightTouchEnabled(true);
+		//settings.setBuiltInZoomControls(true);  // 読み込んだWebページをWebView上で拡大・縮小（ピンチイン・アウト）可能に
+		settings.setJavaScriptEnabled(true);  //javascript有効化
+        
+        webView.loadUrl("http://www.nikkei.com/");
+    }
+    
+    //戻るボタンで前のページに戻る
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	  //戻るボタンで前のページに戻る
+         if(keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+        	 webView.goBack();
+             return true;
+         }
+         return super.onKeyDown(keyCode, event);
+    }
+    // オプション・メニューを作成
+
+
+    private static final int SUB_MENU_NEWS = 0;
+	    private static final int NIKKEI_NEWS_MENU_ID = 1;
+	    private static final int GOOGLE_NEWS_MENU_ID = 2;
+	    private static final int HOTENTRY_MENU_ID = 3;
+	    private static final int GOOGLE_MENU_ID = 4;
+    private static final int SUB_MENU_BLOG = 5;
+	    private static final int ANDROID_DEV_BLOG_SEARCH = 6;
+	    private static final int NEXUS7_BLOG_SEARCH = 7;
+	    private static final int ANDROID_BLOG_SEARCH = 8;
+    private static final int SUB_MENU_BOOKMARK = 9;
+	    private static final int ADD_BOOKMARK_MENU_ID = 10;
+	    private static final int VIEW_BOOKMARK_MENU_ID = 11;
+    private static final int FINISH_MENU_ID = 12;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+
+        // ニュースはサブメニューにする
+        SubMenu subMenuNews;
+        subMenuNews = menu.addSubMenu(Menu.NONE, SUB_MENU_NEWS, 0, R.string.subMenu_news);
+        subMenuNews.setIcon(android.R.drawable.ic_menu_directions);
+        
+	        // 日経 メニュー・アイテム
+	        subMenuNews.add(0, NIKKEI_NEWS_MENU_ID, 0, R.string.nikkei);
+	        // Googleニュース メニュー・アイテム
+	        subMenuNews.add(0, GOOGLE_NEWS_MENU_ID, 1, R.string.google_news);
+	        // 人気エントリー メニュー・アイテム
+	        subMenuNews.add(0, HOTENTRY_MENU_ID, 2, R.string.hotentry);
+	        
+        // Google メニュー・アイテム
+        menu.add(0, GOOGLE_MENU_ID, 1, R.string.google);
+
+        // ブログ検索はサブメニューにする
+        SubMenu subMenuBlog;
+        subMenuBlog = menu.addSubMenu(Menu.NONE, SUB_MENU_BLOG, 2, R.string.subMenu_blog_search);
+        subMenuBlog.setIcon(android.R.drawable.ic_menu_directions);
+        
+	        // android 開発 ブログ検索
+	        subMenuBlog.add(0, ANDROID_DEV_BLOG_SEARCH, 0, R.string.android_dev_blog_seach);
+	        // Nexus7 ブログ検索
+	        subMenuBlog.add(0, NEXUS7_BLOG_SEARCH, 0, R.string.nexus7_blog_seach);
+	        // Android ブログ検索
+	        subMenuBlog.add(0, ANDROID_BLOG_SEARCH, 0, R.string.android_blog_seach);
+        
+        // ブックマークはサブメニューにする
+        SubMenu subMenuBookmark;
+        subMenuBookmark = menu.addSubMenu(Menu.NONE, SUB_MENU_BOOKMARK, 3, R.string.subMenu_bookmark);
+        subMenuBookmark.setIcon(android.R.drawable.ic_menu_directions);
+	
+		     // サブメニューにメニューアイテムを追加する
+	        subMenuBookmark.add(Menu.NONE,  ADD_BOOKMARK_MENU_ID, 0, R.string.add_bookmark);
+	        subMenuBookmark.add(Menu.NONE, VIEW_BOOKMARK_MENU_ID, 1, R.string.view_bookmark);
+        
+        // 終了 　メニュー・アイテム
+        menu.add(0, FINISH_MENU_ID, 8, R.string.finish);
+        
+     
+        
+        return true;
+    }
+    
+    // メニューを選択した時の処理
+    public boolean onMenuItemSelected(int featureId,MenuItem item){
+        switch (item.getItemId()) {
+        case NIKKEI_NEWS_MENU_ID :
+        	webView.loadUrl("http://www.nikkei.com/");
+        	return true;
+        case GOOGLE_NEWS_MENU_ID :
+        	webView.loadUrl("http://news.google.co.jp/");
+        	return true;
+        case HOTENTRY_MENU_ID :
+        	webView.loadUrl("http://slx.heteml.jp/hatebu/sp");
+        	return true;
+        case GOOGLE_MENU_ID :
+        	webView.loadUrl("http://www.google.co.jp");
+        	return true;
+        case ANDROID_DEV_BLOG_SEARCH :
+        	webView.loadUrl("https://www.google.co.jp/search?q=android+%E9%96%8B%E7%99%BA#q=android+%E9%96%8B%E7%99%BA&hl=ja&tbo=d&source=lnms&tbm=blg&sa=X&ei=e7ruULndN8eHkQXav4BQ&ved=0CBEQ_AUoAA&bav=on.2,or.r_gc.r_pw.r_qf.&bvm=bv.1357700187,d.dGI&fp=4a7b87b2fcf8f3fc&biw=1280&bih=687");
+        	return true;
+        case NEXUS7_BLOG_SEARCH :
+        	webView.loadUrl("https://www.google.co.jp/search?q=android+%E9%96%8B%E7%99%BA#hl=ja&gs_rn=1&gs_ri=serp&gs_is=1&pq=android%20%E9%96%8B%E7%99%BA&cp=4&gs_id=19&xhr=t&q=nexus7&es_nrs=true&pf=p&tbo=d&tbm=blg&sclient=psy-ab&oq=nexu&gs_l=&pbx=1&bav=on.2,or.r_gc.r_pw.r_qf.&fp=13d9d789849eb92e&biw=1280&bih=629");	
+        	return true;
+        case ANDROID_BLOG_SEARCH :
+        	webView.loadUrl("https://www.google.co.jp/search?q=android+%E9%96%8B%E7%99%BA#hl=ja&tbo=d&tbm=blg&sclient=psy-ab&q=android&oq=android&gs_l=serp.3..0l8.414037.415041.4.415245.7.5.0.2.2.1.605.1488.1j1j1j0j1j1.5.0...0.0...1c.1j4.ZkHRPiMpKKI&pbx=1&bav=on.2,or.r_gc.r_pw.r_qf.&fp=13d9d789849eb92e&biw=1280&bih=623");	
+        	return true;
+        case ADD_BOOKMARK_MENU_ID :
+        	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            // アラートダイアログのタイトルを設定します
+        	alertDialogBuilder.setTitle("ブックマーク追加");
+            // アラートダイアログのメッセージを設定します
+        	//alertDialogBuilder.setMessage(currentUrl);
+        	alertDialogBuilder.setMessage(currentTitle);
+            // アラートダイアログの肯定ボタンがクリックされた時に呼び出されるコールバックリスナーを登録します
+        	alertDialogBuilder.setPositiveButton("追加",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        	// Write
+                            {
+                            	FileOutputStream fileOutputStream = null;
+                                try {
+                                    fileOutputStream = openFileOutput(bookmarkFileName, MODE_APPEND);  //追記モード
+                                    //FileOutputStream fileOutputStream = openFileOutput(bookmarkFileName, MODE_PRIVATE); 
+                                    String writeString = currentTitle + "," + currentUrl +  "\n";
+                                    fileOutputStream.write(writeString.getBytes());
+
+                                } catch (FileNotFoundException e) {
+                                } catch (IOException e) {
+                                }
+                                finally {
+                                    if (fileOutputStream != null) {
+                                    	try{
+                                    	fileOutputStream.close();
+                                    	}
+                                    	catch(Exception ex) {
+                                    	}
+                                    }
+                                }
+                            }
+                            
+                        	Toast.makeText(getApplicationContext(),"追加:" + currentUrl, Toast.LENGTH_LONG).show();
+                        }
+                    });
+            // アラートダイアログの否定ボタンがクリックされた時に呼び出されるコールバックリスナーを登録します
+       	alertDialogBuilder.setNegativeButton("キャンセル",
+       			new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+            // アラートダイアログのキャンセルが可能かどうかを設定します
+        	alertDialogBuilder.setCancelable(true);
+        	AlertDialog alertDialog = alertDialogBuilder.create();
+            // アラートダイアログを表示します
+        	alertDialog.show();
+            
+        	return true;
+        case VIEW_BOOKMARK_MENU_ID :
+			// 起動先アクティビティからデータを返してもらいたい場合は
+			// Activity#startActivityForResult(intent, requestCode) を使う
+			// 第二引数の requestCode は 下の onActivityResult の
+			// 第一引数に渡される値で、条件分岐のための数値。
+			// requestCode が 0 未満の場合は startActivity(intent) と等価
+			Intent intent =new Intent(this, BookmarkActivity.class);
+			int requestCode = 123;
+			startActivityForResult(intent, requestCode);
+			return true;
+        case FINISH_MENU_ID :
+        	//終了ボタンが押されたとき
+        	webView.clearCache(true); // キャッシュのクリア
+        	webView.clearHistory(); // 履歴のクリア
+        	finish();  //終了
+           return true;
+        default :
+            break;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+    
+    // ブックマーク画面から戻ってきた時の処理
+    // startActivityForResult で起動させたアクティビティが
+    // finish() により破棄されたときにコールされる
+    // requestCode : startActivityForResult の第二引数で指定した値が渡される
+    // resultCode : 起動先のActivity.setResult の第一引数が渡される
+    // Intent data : 起動先Activityから送られてくる Intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+	    
+	    switch (requestCode) {
+	    case 123:
+	        if (resultCode == RESULT_OK) {
+	        	Bundle bundle = data.getExtras();
+	        	webView.loadUrl(bundle.getString("key.StringData"));
+	        } else if (resultCode == RESULT_CANCELED) {
+
+	        }
+	        break;
+	
+	    default:
+	        break;
+	    }
+
+    }
+    
+    // 画面の向きを切り替えた時に画面を保持する
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+    	super.onConfigurationChanged(newConfig);
+    }
+}
